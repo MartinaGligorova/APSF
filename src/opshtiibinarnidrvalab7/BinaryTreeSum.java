e7ece221103437bf8f2f44cddeed3385cbec0b82
Mgiooa <martina.gligorova1@gmail.com>
2020-12-30 11:21:16 +0100

    Update

--- src/opshtiibinarnidrvalab7/BinaryTreeSum.java
+++ src/opshtiibinarnidrvalab7/BinaryTreeSum.java
@@ -2,83 +2,9 @@ package opshtiibinarnidrvalab7;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 
-
-interface Stack<E> {
-
-    // Elementi na stekot se objekti od proizvolen tip.
-
-    // Metodi za pristap:
-
-    public boolean isEmpty();
-    // Vrakja true ako i samo ako stekot e prazen.
-
-    public E peek();
-    // Go vrakja elementot na vrvot od stekot.
-
-    // Metodi za transformacija:
-
-    public void clear();
-    // Go prazni stekot.
-
-    public void push(E x);
-    // Go dodava x na vrvot na stekot.
-
-    public E pop();
-    // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
-}
-
-class ArrayStack<E> implements Stack<E> {
-    private E[] elems;
-    private int depth;
-
-    @SuppressWarnings("unchecked")
-    public ArrayStack(int maxDepth) {
-        // Konstrukcija na nov, prazen stek.
-        elems = (E[]) new Object[maxDepth];
-        depth = 0;
-    }
-
-
-    public boolean isEmpty() {
-        // Vrakja true ako i samo ako stekot e prazen.
-        return (depth == 0);
-    }
-
-
-    public E peek() {
-        // Go vrakja elementot na vrvot od stekot.
-        if (depth == 0)
-            throw new NoSuchElementException();
-        return elems[depth - 1];
-    }
-
-
-    public void clear() {
-        // Go prazni stekot.
-        for (int i = 0; i < depth; i++) elems[i] = null;
-        depth = 0;
-    }
-
-
-    public void push(E x) {
-        // Go dodava x na vrvot na stekot.
-        elems[depth++] = x;
-    }
-
-
-    public E pop() {
-        // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
-        if (depth == 0)
-            throw new NoSuchElementException();
-        E topmost = elems[--depth];
-        elems[depth] = null;
-        return topmost;
-    }
-}
 
 public class BinaryTreeSum {
 
--- src/opshtiibinarnidrvalab7/PreorderNonRecursive.java
+++ src/opshtiibinarnidrvalab7/PreorderNonRecursive.java
@@ -5,37 +5,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 
-class BNode<E> {
-
-    public E info;
-    public BNode<E> left;
-    public BNode<E> right;
-
-    static int LEFT = 1;
-    static int RIGHT = 2;
-
-    public BNode(E info) {
-        this.info = info;
-        left = null;
-        right = null;
-    }
-
-    public BNode() {
-        this.info = null;
-        left = null;
-        right = null;
-    }
-
-    public BNode(E info, BNode<E> left, BNode<E> right) {
-        this.info = info;
-        this.left = left;
-        this.right = right;
-    }
-
-}
-
-
-
 public class PreorderNonRecursive {
 
     public static void main(String[] args) throws Exception {
