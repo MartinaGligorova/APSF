7a9f214057810eb05a13d50dfdbab737706957b3
Mgiooa <martina.gligorova1@gmail.com>
2021-01-21 22:06:29 +0100

    update

--- src/hashingavexample/SimpleEmailSystem.java
+++ src/hashingavexample/SimpleEmailSystem.java
@@ -47,6 +47,120 @@ class SLLNode<E> {
     }
 }
 
+class CBHT<K extends Comparable<K>, E> {
+
+    // An object of class CBHT is a closed-bucket hash table, containing
+    // entries of class MapEntry.
+    private SLLNode<MapEntry<K, E>>[] buckets;
+
+    @SuppressWarnings("unchecked")
+    public CBHT(int m) {
+        // Construct an empty CBHT with m buckets.
+        buckets = (SLLNode<MapEntry<K, E>>[]) new SLLNode[m];
+    }
+
+    private int hash(K key) {
+        // Napishete ja vie HASH FUNKCIJATA
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+
+    public SLLNode<MapEntry<K, E>> search(K targetKey) {
+        // Find which if any node of this CBHT contains an entry whose key is
+        // equal
+        // to targetKey. Return a link to that node (or null if there is none).
+        int b = hash(targetKey);
+        for (SLLNode<MapEntry<K, E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (targetKey.equals(((MapEntry<K, E>) curr.element).key))
+                return curr;
+        }
+        return null;
+    }
+
+    public void insert(K key, E val) {        // Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K, E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (key.equals(((MapEntry<K, E>) curr.element).key)) {
+                // Make newEntry replace the existing entry ...
+                curr.element = newEntry;
+                return;
+            }
+        }
+        // Insert newEntry at the front of the 1WLL in bucket b ...
+        buckets[b] = new SLLNode<MapEntry<K, E>>(newEntry, buckets[b]);
+    }
+
+    // modified - added method 2
+    public void insert2(K key, E val) {        // Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K, E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (val.equals(((MapEntry<K, E>) curr.element).value)) {
+                // Make newEntry replace the existing entry ...
+                // dokolku values se ednakvi i.e iminjata
+                curr.element = newEntry;
+                return;
+            }
+        }
+        // Insert newEntry at the front of the 1WLL in bucket b ...
+        if (buckets[b] == null) {
+            buckets[b] = new SLLNode<MapEntry<K, E>>(newEntry, buckets[b]);
+        } else {
+            SLLNode<MapEntry<K, E>> pok = buckets[b];
+            while (pok.succ != null) {
+                pok = pok.succ;
+            }
+            // pok.succ = null
+            pok.succ = new SLLNode<>(newEntry, pok.succ);
+        }
+
+    }
+
+    // modified - added method
+    public void insert1(K key, E val) {        // Insert the entry <key, val> into this CBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        for (SLLNode<MapEntry<K, E>> curr = buckets[b]; curr != null; curr = curr.succ) {
+            if (val.equals(((MapEntry<K, E>) curr.element).value)) {
+                // Make newEntry replace the existing entry ...
+                // dokolku values == togash overwrite
+                curr.element = newEntry;
+                return;
+            }
+        }
+        // Insert newEntry at the front of the 1WLL in bucket b ...
+        buckets[b] = new SLLNode<MapEntry<K, E>>(newEntry, buckets[b]);
+    }
+
+
+    public void delete(K key) {
+        // Delete the entry (if any) whose key is equal to key from this CBHT.
+        int b = hash(key);
+        for (SLLNode<MapEntry<K, E>> pred = null, curr = buckets[b]; curr != null; pred = curr, curr = curr.succ) {
+            if (key.equals(((MapEntry<K, E>) curr.element).key)) {
+                if (pred == null)
+                    buckets[b] = curr.succ;
+                else
+                    pred.succ = curr.succ;
+                return;
+            }
+        }
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            for (SLLNode<MapEntry<K, E>> curr = buckets[i]; curr != null; curr = curr.succ) {
+                temp += curr.element.toString() + " ";
+            }
+            temp += "\n";
+        }
+        return temp;
+    }
+
+}
 
 
 public class SimpleEmailSystem implements Comparable<SimpleEmailSystem> {
@@ -57,7 +171,6 @@ public class SimpleEmailSystem implements Comparable<SimpleEmailSystem> {
     String naslov;
     String datumVreme;
     Category kategorija;
-
 
 
     public SimpleEmailSystem(String naslovVlez, String datumVremeVlez) {
@@ -71,7 +184,6 @@ public class SimpleEmailSystem implements Comparable<SimpleEmailSystem> {
         this.datumVreme = datumVreme;
         this.kategorija = _kategorija;
     }
-
 
 
     public int hashCode() {
@@ -185,7 +297,7 @@ public class SimpleEmailSystem implements Comparable<SimpleEmailSystem> {
 
             SLLNode<MapEntry<SimpleEmailSystem, Integer>> najdi = tabela.search(obj); // spored kluch
             // hash funkcija od klucot - naslovot i datum i vreme --> koficka
-            if(najdi == null)
+            if (najdi == null)
                 continue;
 
             System.out.println(najdi.element.key.kategorija + " " + najdi.element.value);
